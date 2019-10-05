827f9d7dc53b60d5b826dc8bcdf96c9a6e85bffe
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-10-05 13:43:27 +0100

    typescript

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const path = require("path")
 var path_1 = __importDefault(require("path"));
 function getNodeModulesDir() {
     var dir = path_1.default.resolve(__dirname, "..");
